82ea76a07a3fe3333be7713e5a5928d55c85c069
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-11 18:44:19 +0530

    saving orders to firestore

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -8,6 +8,7 @@ import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "../../contextApi/reducer";
 import axios from "../../api/axios";
+import { db } from "../../firebase/Firebase";
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
 
@@ -34,6 +35,15 @@ function Payment() {
       })
       .then(({ paymentIntent }) => {
         //paymentIntent = payment confirmation
+        db.collection("users")
+          .doc(user?.uid)
+          .collection("orders")
+          .doc(paymentIntent.id)
+          .set({
+            basket: basket,
+            amount: paymentIntent.amount,
+            created: paymentIntent.created,
+          });
         setSucceeded(true);
         setError(null);
         setProccessing(false);
